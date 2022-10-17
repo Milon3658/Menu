4d89b62e35a5d3114fae68f26c3f19fc124235f2
Milon <jan.milon.56@gmail.com>
2022-10-18 01:48:41 +0600

    another section done

---
+++ src/component/Eat/Eat.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import eating from '../../images/tiffin.png'
+import left from '../../images/left.png'
+import right from '../../images/right.png'
+import './Eat.css'
+const Eat = () => {
+    return (
+        <div className='flexible-container'>
+            <img src={eating} alt="" />
+            <div className='colonel'>
+                <div>
+                    <img src={left} alt="" />
+                    <img src={right} alt="" />
+                </div>
+                <h1>
+                    Colonel Saab <br /> Covent Garden
+                </h1>
+                <p>OFFERS AND EVENTS AT <br /> RESTAURANT COVENT GARDEN</p>
+                <small>Discover the latest offers and events at Colonel Saab.</small>
+                <button>What's on</button>
+            </div>
+        </div>
+    );
+};
+
+export default Eat;
--- src/component/Main/Main.js
+++ src/component/Main/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Eat from '../Eat/Eat';
 import Story from '../Story/Story';
 
 const Main = () => {
     return (
         <div>
             <Story></Story>
+            <Eat></Eat>
         </div>
     );
 };
